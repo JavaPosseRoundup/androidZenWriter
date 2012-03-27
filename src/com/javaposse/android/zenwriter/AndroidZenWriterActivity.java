710614be1139387947dcc6299072283d3bfd1483
justin.fiore <justin.fiore@gmail.com>
2012-03-27 17:14:39 -0600

    Reformatted all java files. Removed SelectImageListener as it isn't used anymore.

--- src/com/javaposse/android/zenwriter/AndroidZenWriterActivity.java
+++ src/com/javaposse/android/zenwriter/AndroidZenWriterActivity.java
@@ -28,233 +28,213 @@ import android.widget.Toast;
 
 public class AndroidZenWriterActivity extends SherlockActivity {
 
-	public static String currentFilename = "current.txt";
-	public static String settingsFilename = "settings.properties";
+    public static String currentFilename = "current.txt";
+    public static String settingsFilename = "settings.properties";
 
-	public static final int SELECT_PHOTO = 100;
-	private static final boolean ACTIONBAR = false;
+    public static final int SELECT_PHOTO = 100;
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	        if (ACTIONBAR) {
-	            setTheme(R.style.Theme_Sherlock_Light);
-	        }
-		super.onCreate(savedInstanceState);
-		//loadSettings();
-		setContentView(R.layout.main);
-		ViewPager pager = (ViewPager) findViewById(R.id.ViewPager1);
-		pager.setAdapter(new ZenAdapter(this));
-		pager.setCurrentItem(1, true);
-		
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_Sherlock_Light_NoActionBar);
+        super.onCreate(savedInstanceState);
+        // loadSettings();
+        setContentView(R.layout.main);
+        ViewPager pager = (ViewPager) findViewById(R.id.ViewPager1);
+        pager.setAdapter(new ZenAdapter(this));
+        pager.setCurrentItem(1, true);
+
         applyPreferences();
-        
-		
-	}
 
-	public static final int EDIT_PREFERENCES = 1;
+    }
 
-	public void openThemeSettings(View parent) {
-		Intent settingsActivity = new Intent(this, PrefsActivity.class);
-		startActivityForResult(settingsActivity, EDIT_PREFERENCES);
-	}
+    public static final int EDIT_PREFERENCES = 1;
 
-	public void shareStuff() {
-		Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-		sharingIntent.setType("text/plain");
-		
-		EditText editText = (EditText) findViewById(R.id.editText1);
-		String content = editText.getText().toString();
+    public void openThemeSettings(View parent) {
+        Intent settingsActivity = new Intent(this, PrefsActivity.class);
+        startActivityForResult(settingsActivity, EDIT_PREFERENCES);
+    }
 
-		sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "TODO NEED Subject!!!");
-		sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, content);
-		
-		startActivity(Intent.createChooser(sharingIntent, "Share via"));
-	}
-	
-	@Override
-	protected void onPause() {
-		super.onPause();
-		saveFile(currentFilename);
-	}
+    public void shareStuff() {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		//saveSettings();
-		saveFile(currentFilename);
-	}
+        EditText editText = (EditText) findViewById(R.id.editText1);
+        String content = editText.getText().toString();
 
-	protected void saveFile(String filename) {
-		FileOutputStream fos = null;
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+                "TODO NEED Subject!!!");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, content);
 
-		EditText editText = (EditText) findViewById(R.id.editText1);
-		String content = editText.getText().toString();
-		try {
-			fos = openFileOutput(filename, MODE_PRIVATE);
-			fos.write(content.getBytes());
-		} catch (IOException e) {
-			Log.e("SaveFile", "Failed to save file: ", e);
-		} finally {
-			if (fos != null) {
-				try {
-					fos.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-		Toast.makeText(this, "Saved file", Toast.LENGTH_LONG).show();
-	}
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+    }
 
-	protected void loadFile(String filename) {
-		FileInputStream fis = null;
-		BufferedReader br = null;
-		File file = getFileStreamPath(filename);
-		StringBuilder content = new StringBuilder();
-		if (file.exists()) {
-			EditText editText = (EditText) findViewById(R.id.editText1);
-			try {
-				fis = openFileInput(filename);
-				br = new BufferedReader(new InputStreamReader(fis));
-				while (br.ready()) {
-					content.append(br.readLine()).append("\n");
-				}
-				editText.setText(content.toString());
-				editText.setSelection(content.length());
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveFile(currentFilename);
+    }
 
-			} catch (IOException e) {
-				Log.e("SaveFile", "Failed to save file: ", e);
-			} finally {
-				if (br != null) {
-					try {
-						br.close();
-					} catch (IOException e) {
-					}
-				}
-				if (fis != null) {
-					try {
-						fis.close();
-					} catch (IOException e) {
-					}
-				}
-			}
-		}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // saveSettings();
+        saveFile(currentFilename);
+    }
 
-	}
-	
+    protected void saveFile(String filename) {
+        FileOutputStream fos = null;
 
-	protected void applyPreferences() {
-	    View top = findViewById(R.id.Top);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        EditText editText = (EditText) findViewById(R.id.editText1);
+        if(editText != null) {
+            String content = editText.getText().toString();
+            try {
+                fos = openFileOutput(filename, MODE_PRIVATE);
+                fos.write(content.getBytes());
+            } catch (IOException e) {
+                Log.e("SaveFile", "Failed to save file: ", e);
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+            Toast.makeText(this, "Saved file", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void loadFile(String filename) {
+        FileInputStream fis = null;
+        BufferedReader br = null;
+        File file = getFileStreamPath(filename);
+        StringBuilder content = new StringBuilder();
+        if (file.exists()) {
+            EditText editText = (EditText) findViewById(R.id.editText1);
+            try {
+                fis = openFileInput(filename);
+                br = new BufferedReader(new InputStreamReader(fis));
+                while (br.ready()) {
+                    content.append(br.readLine()).append("\n");
+                }
+                editText.setText(content.toString());
+                editText.setSelection(content.length());
+
+            } catch (IOException e) {
+                Log.e("SaveFile", "Failed to save file: ", e);
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                    }
+                }
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        }
+
+    }
+
+    protected void applyPreferences() {
+        View top = findViewById(R.id.Top);
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
         String backgroundImage = prefs.getString("backgroundpref", "");
-        Log.i("AndroidZenWriter:applyPreferences", "BackgroundImage: " + backgroundImage);
-        if(!backgroundImage.equals("")) {
+        Log.i("AndroidZenWriter:applyPreferences", "BackgroundImage: "
+                + backgroundImage);
+        if (!backgroundImage.equals("")) {
             Drawable background = getDrawable(this, backgroundImage);
-            if(background != null) {
+            if (background != null) {
                 top.setBackgroundDrawable(background);
             }
         }
         // TODO: Apply other preferences
-	}
-	
+    }
+
     protected void onActivityResult(int requestCode, int resultCode,
             Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-        if(requestCode == EDIT_PREFERENCES && resultCode == RESULT_OK) {
+        if (requestCode == EDIT_PREFERENCES && resultCode == RESULT_OK) {
             this.applyPreferences();
         }
-        
+
     }
-/*
-	public void saveSettings() {
-		FileOutputStream fos = null;
 
-		try {
-			fos = openFileOutput(settingsFilename, MODE_PRIVATE);
-			settings.save(fos, "Settings saved at: " + new Date());
-		} catch (IOException e) {
-			Log.e("SaveFile", "Failed to save file: ", e);
-		} finally {
-			if (fos != null) {
-				try {
-					fos.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-		Toast.makeText(this, "Saved settings", Toast.LENGTH_LONG).show();
-	}
+    /*
+     * public void saveSettings() { FileOutputStream fos = null;
+     * 
+     * try { fos = openFileOutput(settingsFilename, MODE_PRIVATE);
+     * settings.save(fos, "Settings saved at: " + new Date()); } catch
+     * (IOException e) { Log.e("SaveFile", "Failed to save file: ", e); }
+     * finally { if (fos != null) { try { fos.close(); } catch (IOException e) {
+     * } } } Toast.makeText(this, "Saved settings", Toast.LENGTH_LONG).show(); }
+     * 
+     * public void loadSettings() { FileInputStream fis = null;
+     * 
+     * File settingsFile = getFileStreamPath(settingsFilename); if
+     * (settingsFile.exists()) { try { fis = openFileInput(settingsFilename);
+     * settings.load(fis); } catch (IOException e) { Log.e("SaveFile",
+     * "Failed to save file: ", e); } finally { if (fis != null) { try {
+     * fis.close(); } catch (IOException e) { } } } } Toast.makeText(this,
+     * "Loaded settings", Toast.LENGTH_LONG).show(); }
+     */
+    public static Drawable getDrawable(Context context, String filename) {
 
-	public void loadSettings() {
-		FileInputStream fis = null;
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inSampleSize = 2;
+        Bitmap backgroundBitmap = BitmapFactory.decodeFile(context
+                .getFileStreamPath(filename).getAbsolutePath(), opts);
 
-		File settingsFile = getFileStreamPath(settingsFilename);
-		if (settingsFile.exists()) {
-			try {
-				fis = openFileInput(settingsFilename);
-				settings.load(fis);
-			} catch (IOException e) {
-				Log.e("SaveFile", "Failed to save file: ", e);
-			} finally {
-				if (fis != null) {
-					try {
-						fis.close();
-					} catch (IOException e) {
-					}
-				}
-			}
-		}
-		Toast.makeText(this, "Loaded settings", Toast.LENGTH_LONG).show();
-	}
-*/
-	public static Drawable getDrawable(Context context, String filename) {
+        if (backgroundBitmap != null) {
+            BitmapDrawable drawable = new BitmapDrawable(backgroundBitmap);
+            return drawable;
 
-		BitmapFactory.Options opts = new BitmapFactory.Options();
-		opts.inSampleSize = 2;
-		Bitmap backgroundBitmap = BitmapFactory.decodeFile(
-				context.getFileStreamPath(filename).getAbsolutePath(), opts);
+        }
+        return null;
 
-		if (backgroundBitmap != null) {
-			BitmapDrawable drawable = new BitmapDrawable(backgroundBitmap);
-			return drawable;
+    }
 
-		}
-		return null;
+    public static final int SHARE_CONTEXT_MENU_ITEMID = 100;
 
-	}
-	
-	public static final int SHARE_CONTEXT_MENU_ITEMID = 100;
-	
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-	    super.onContextItemSelected(item);
-	    
-	    if(item.getItemId() == SHARE_CONTEXT_MENU_ITEMID) {
-	        shareStuff();
-	    }
-	    
-	    return true;
-	}
-	
-	
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+
+        if (item.getItemId() == SHARE_CONTEXT_MENU_ITEMID) {
+            shareStuff();
+        }
+
+        return true;
+    }
 
     // Action Bar
     @Override
     public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
-        if (!ACTIONBAR) {
-            return false;
-        }
-	menu.add("Edit").setIcon(android.R.drawable.ic_menu_edit)
-		.setShowAsAction(com.actionbarsherlock.view.MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add("Edit")
+                .setIcon(android.R.drawable.ic_menu_edit)
+                .setShowAsAction(
+                        com.actionbarsherlock.view.MenuItem.SHOW_AS_ACTION_ALWAYS);
 
-	menu.add("Search").setIcon(android.R.drawable.ic_menu_search)
-		.setShowAsAction(com.actionbarsherlock.view.MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add("Search")
+                .setIcon(android.R.drawable.ic_menu_search)
+                .setShowAsAction(
+                        com.actionbarsherlock.view.MenuItem.SHOW_AS_ACTION_ALWAYS);
 
-	menu.add("Save").setIcon(android.R.drawable.ic_menu_save)
-		.setShowAsAction(com.actionbarsherlock.view.MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        menu.add("Save")
+                .setIcon(android.R.drawable.ic_menu_save)
+                .setShowAsAction(
+                        com.actionbarsherlock.view.MenuItem.SHOW_AS_ACTION_IF_ROOM);
 
-	menu.add("Share").setIcon(android.R.drawable.ic_menu_share)
-		.setShowAsAction(com.actionbarsherlock.view.MenuItem.SHOW_AS_ACTION_IF_ROOM);
-	return true;
+        menu.add("Share")
+                .setIcon(android.R.drawable.ic_menu_share)
+                .setShowAsAction(
+                        com.actionbarsherlock.view.MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return true;
     }
 }
--- src/com/javaposse/android/zenwriter/ZenAdapter.java
+++ src/com/javaposse/android/zenwriter/ZenAdapter.java
@@ -17,104 +17,107 @@ import android.widget.TextView;
 
 public class ZenAdapter extends PagerAdapter {
 
-	private AndroidZenWriterActivity context;
+    private AndroidZenWriterActivity context;
 
-	public ZenAdapter(AndroidZenWriterActivity context) {
-		super();
-		this.context = context;
-	}
+    public ZenAdapter(AndroidZenWriterActivity context) {
+        super();
+        this.context = context;
+    }
 
-	@Override
-	public int getCount() {
-		return 3;
-	}
+    @Override
+    public int getCount() {
+        return 3;
+    }
 
-	@Override
-	public Object instantiateItem(ViewGroup container, int position) {
-		View view;
-		LayoutInflater inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view;
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-		switch (position) {
+        switch (position) {
 
-		case 1:
-			view = inflater.inflate(R.layout.editview, null);
-			break;
-		case 2:
-			view = inflater.inflate(R.layout.thememanager, null);
-			break;
-		case 0:
-		default:
-			TextView textView = new TextView(context);
-			textView.setText("" + position);
-			textView.setTextSize(50);
-			textView.setTextColor(Color.RED);
-			view = textView;
-			break;
-		}
+        case 1:
+            view = inflater.inflate(R.layout.editview, null);
+            break;
+        case 2:
+            view = inflater.inflate(R.layout.thememanager, null);
+            break;
+        case 0:
+        default:
+            TextView textView = new TextView(context);
+            textView.setText("" + position);
+            textView.setTextSize(50);
+            textView.setTextColor(Color.RED);
+            view = textView;
+            break;
+        }
 
-		container.addView(view);
+        container.addView(view);
 
-		if (position == 1) {
-			context.loadFile(AndroidZenWriterActivity.currentFilename);
-			EditText editor = (EditText) view.findViewById(R.id.editText1);
-	        View.OnCreateContextMenuListener editorOnContextMenuCreateListener = new View.OnCreateContextMenuListener() {
+        if (position == 1) {
+            context.loadFile(AndroidZenWriterActivity.currentFilename);
+            EditText editor = (EditText) view.findViewById(R.id.editText1);
+            View.OnCreateContextMenuListener editorOnContextMenuCreateListener = new View.OnCreateContextMenuListener() {
 
-	            public void onCreateContextMenu(ContextMenu menu, View view, ContextMenuInfo menuInfo) {
-	                menu.add(Menu.NONE, AndroidZenWriterActivity.SHARE_CONTEXT_MENU_ITEMID, Menu.NONE, "Share");
-	            }
-	        };
-	        editor.setOnCreateContextMenuListener(editorOnContextMenuCreateListener);
-		}
+                public void onCreateContextMenu(ContextMenu menu, View view,
+                        ContextMenuInfo menuInfo) {
+                    menu.add(Menu.NONE,
+                            AndroidZenWriterActivity.SHARE_CONTEXT_MENU_ITEMID,
+                            Menu.NONE, "Share");
+                }
+            };
+            editor.setOnCreateContextMenuListener(editorOnContextMenuCreateListener);
+        }
 
-		if (position == 2) {
-			// initialize the gallery
-			Gallery gallery = (Gallery) context.findViewById(R.id.themegallery);
+        if (position == 2) {
+            // initialize the gallery
+            Gallery gallery = (Gallery) context.findViewById(R.id.themegallery);
 
-			gallery.setAdapter(new ImageAdapter(context));
+            gallery.setAdapter(new ImageAdapter(context));
 
-			gallery.setOnItemClickListener(new OnItemClickListener() {
-				public void onItemClick(AdapterView parent, View v,
-						int position, long id) {
-					// find the layout object
-					View top = context.findViewById(R.id.Top);
+            gallery.setOnItemClickListener(new OnItemClickListener() {
+                public void onItemClick(AdapterView parent, View v,
+                        int position, long id) {
+                    // find the layout object
+                    View top = context.findViewById(R.id.Top);
 
-					int selected = 0;
+                    int selected = 0;
 
-					switch (position) {
-					case 0:
-						selected = R.drawable.beach1;
-						break;
-					case 1:
-						selected = R.drawable.kauai;
-						break;
-					case 2:
-						selected = R.drawable.mirror;
-						break;
-					case 3:
-						selected = R.drawable.napali;
-						break;
-					default:
-						selected = R.drawable.stonearch;
-						break;
-					}
+                    switch (position) {
+                    case 0:
+                        selected = R.drawable.beach1;
+                        break;
+                    case 1:
+                        selected = R.drawable.kauai;
+                        break;
+                    case 2:
+                        selected = R.drawable.mirror;
+                        break;
+                    case 3:
+                        selected = R.drawable.napali;
+                        break;
+                    default:
+                        selected = R.drawable.stonearch;
+                        break;
+                    }
 
-					top.setBackgroundResource(selected);
-				}
-			});
-		}
+                    top.setBackgroundResource(selected);
+                }
+            });
+        }
 
-		return view;
-	}
+        return view;
+    }
 
-	@Override
-	public void destroyItem(ViewGroup container, int position, Object object) {
-		container.removeView((View) object);
-	}
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
 
-	@Override
-	public boolean isViewFromObject(View arg0, Object arg1) {
-		return arg0 == arg1;
-	}
+    @Override
+    public boolean isViewFromObject(View arg0, Object arg1) {
+        return arg0 == arg1;
+    }
 
 }
